4a5b57fedc8416fa55c7dce178cced1c361a8eb7
DoKyu Lee <dokyulee@DK-MacBook-Air.local>
2024-11-07 12:52:59 -0500

    email

--- CUSTOM.js
+++ CUSTOM.js
@@ -11,11 +11,11 @@ const phrase4 = `Dream your Future`;
 const phrase5 = `Illustrate Portfolio`;
 const phrase6 = `Design Solutions`;
 
-const email = `mickeyfromsd@gmail.com`;
+const email = `info@penfromtheisland.com`;
 const phone = `(123) 456-7890`;
 
 const messageSuccess = `Your request is successfully sent.`;
-const messageFail = `Sorry my mailbox is not set up yet. Please email us at info@rysonconsulting.com`;
+const messageFail = `Sorry my mailbox is not set up yet. Please email us at info@penfromtheisland.com`;
 
 const mainDesc = `
     We offer a customized Grpafic Design Portfolio at a minimal cost, providing everything you need to jumpstart your illustration skills up and to light on your buineess visibility. With our service, you can visualize your careers and business within a couple of months.
